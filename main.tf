54586750bb2b0ffe169b926255733c017d5821cf
suzusuke <suzusuke0806@gmail.com>
2023-03-16 08:04:42 +0900

    added vpc create setting

--- main.tf
+++ main.tf
@@ -1,9 +1,23 @@
 provider "aws" {
-  profile = "terraform"
-  region = "ap-northeast-1"
+  access_key = ACCESS_KEY
+  secret_key = SECRET_KEY
+  region     = REGION
 }
 
-resource "aws_instance" "hello-world" {
-  ami = ""
+resource "aws_security_group" "sg_example" {
+  name = sg_example
+  description = this_is_example
+  vpc_id = ''
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_instance" "ecs_example" {
+  vpc_security_group_ids = []
+  ami = "ami-030cf0a1edb8636ab"
   instance_type = "t2.micro"
+  tags = {
+    Name = "example"
+  }
 }
